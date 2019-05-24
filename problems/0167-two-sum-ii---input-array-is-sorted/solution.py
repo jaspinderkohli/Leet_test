e327b30c28c3005e7f80a40a1f73815267d43119
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2019-05-24 06:18:34 +0000

    [LeetCode Sync] Runtime - 48 ms (100.00%), Memory - 12.5 MB (37.19%)

---
+++ problems/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        wanted_nums = {}
+        for i in range(len(numbers)):
+            if numbers[i] in wanted_nums:
+				return [wanted_nums[numbers[i]], i+1]
+            else:
+                wanted_nums[target - numbers[i]] = i+1
+
