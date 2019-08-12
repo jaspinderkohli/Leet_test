e98d867ca42e9de92cd555fa62c9583b5d5fcddf
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2019-08-12 20:21:50 +0000

    [LeetCode Sync] Runtime - 84 ms (20.70%), Memory - 13.7 MB (9.75%)

--- problems/0026-remove-duplicates-from-sorted-array/solution.py
+++ problems/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,15 +1,12 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        '''
-            if doesnt exist in dict means it's unique
-            add it to nums and increase the cntr
-        '''
-        dic = {}
-        cnt = 0
-        for i, x in enumerate(nums):
-            if x not in dic.values():
-                dic[i] = x
-                nums[cnt] = x # use cnt as index
-                cnt+=1
-        return cnt
-        
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        x = 1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                nums[x] = nums[i+1]
+                x+=1
+        return(x)
