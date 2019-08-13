db4dd4e5dae4a9482badeafd364ad41762c974f6
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2019-08-13 08:28:36 +0000

    [LeetCode Sync] Runtime - 16 ms (55.25%), Memory - 11.6 MB (64.33%)

--- problems/0027-remove-element/solution.py
+++ problems/0027-remove-element/solution.py
@@ -1,8 +1,16 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        idx = 0
-        for i,x in enumerate(nums):
-            if x != val:
-                nums[idx] = x
-                idx+=1
-        return idx
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # print nums ,'\n', val
+        x = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                # nums[i] = nums[i]
+                nums[x]=nums[i]
+                x+=1
+                
+        return x
