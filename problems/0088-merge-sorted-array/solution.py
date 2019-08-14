ac325ae60446e2469ade91ea695790f36778027f
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2019-08-14 05:30:52 +0000

    [LeetCode Sync] Runtime - 20 ms (50.96%), Memory - 11.9 MB (9.02%)

--- problems/0088-merge-sorted-array/solution.py
+++ problems/0088-merge-sorted-array/solution.py
@@ -1,34 +1,22 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        # for i in range(m, len(nums1)):
-        #     nums1[i] = nums2[i-m]
-        # return nums1.sort()
-
-        '''
-            O (m+n)
-            
-        '''
-        i, j , k = m -1, n-1 , m+n -1
-        while i >= 0 and j >= 0:
-            print(nums1)
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i-=1
-            else:
-                nums1[k] = nums2[j]
-                j -=1
-
-            k-=1
-
-        # if remaining elements in list
-        while j>=0:
-            nums1[k]= nums2[j]
-            j-=1
-            k-=1
-        
-
-                
+        # print nums1
+        # print m
+        # print nums2
+        # print n
+        x = 0
+        for i in range(m):
+                nums1[x]=nums1[i]
+                x+=1
+        for j in range(len(nums2)):
+            nums1[x]=nums2[j]
+            x+=1
+        return nums1.sort()
         
