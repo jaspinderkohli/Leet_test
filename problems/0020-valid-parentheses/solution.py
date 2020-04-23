ca5b0c4c542238f7c2b3e8e1f8dc29d9a9476183
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2020-04-23 08:23:56 +0000

    [LeetCode Sync] Runtime - 20 ms (20.77%), Memory - 12.9 MB (6.01%)

--- problems/0020-valid-parentheses/solution.py
+++ problems/0020-valid-parentheses/solution.py
@@ -1,20 +1,16 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         stack = []
-        # map the open brackets to their corresponding close brackets
-        bracket_map = {
-            '(': ')',
-            '{': '}',
-            '[': ']'
-        }
-
-        for ch in s:
-            if ch in bracket_map:
-                # if the character is an open bracket, push it onto the stack
-                stack.append(ch)
-            elif len(stack) == 0 or bracket_map[stack.pop()] != ch:
-                # if the stack is empty or the popped element is not the corresponding open bracket for the current close bracket, then the string is not valid
-                return False
-
-        return len(stack) == 0
-
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
