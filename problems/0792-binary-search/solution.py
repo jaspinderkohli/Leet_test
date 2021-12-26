80eecda03b895a570e8941b1b279dacd6b04a5a5
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2021-12-26 19:14:25 +0000

    [LeetCode Sync] Runtime - 236 ms (5.35%), Memory - 15.6 MB (100.00%)

--- problems/0792-binary-search/solution.py
+++ problems/0792-binary-search/solution.py
@@ -1,25 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # for i,x in enumerate(nums):
-        #     if x == target:
-        #         return(i)
-        # return(-1)
-        """ 
-            Using binary serarch 
-            l 0
-            h last element
-        """
-        l=0
-        h=len(nums)-1
-        while l<=h:
-            mid = (l+h)//2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
-                h = mid - 1
-        return -1
-
-            
-
+        for i,x in enumerate(nums):
+            if x == target:
+                return(i)
+        return(-1)
