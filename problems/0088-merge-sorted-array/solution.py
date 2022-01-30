5ddfe584e8305081088d630f50df9273bf983445
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2022-01-30 07:29:13 +0000

    Sync LeetCode submission Runtime - 38 ms (68.83%), Memory - 13.9 MB (100.00%)

--- problems/0088-merge-sorted-array/solution.py
+++ problems/0088-merge-sorted-array/solution.py
@@ -3,13 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        for i in range(m, len(nums1)):
+            nums1[i] = nums2[i-m]
+        return nums1.sort()
         
-        # for i in range(m, len(nums1)):
-        i = m
-        while i < len(nums1):
-            # print(i, nums1[i])
-            for j in range(0,n):
-                nums1[i] = nums2[j]
-                i+=1
-        nums1.sort()
         
