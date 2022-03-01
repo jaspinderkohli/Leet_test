fb3bc10322bf318c4dc3a766894438e5d3419361
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2022-03-01 20:19:54 +0000

    [LeetCode Sync] Runtime - 36 ms (81.60%), Memory - 13.9 MB (100.00%)

--- problems/1528-kids-with-the-greatest-number-of-candies/solution.py
+++ problems/1528-kids-with-the-greatest-number-of-candies/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        res = len(candies) * [None]
-        max_candie = max(candies)
+        out = []
         for i, x in enumerate(candies):
-            res[i] = (x + extraCandies >= max_candie)
-        return res 
+            out.append(x + extraCandies >= max(candies))
+        return out
 
