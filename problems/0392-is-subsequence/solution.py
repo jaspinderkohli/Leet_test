a6902998c5f24509f70ebfa49fbef1815a245d56
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2022-10-17 01:45:49 +0000

    [LeetCode Sync] Runtime - 50 ms (5.65%), Memory - 13.9 MB (100.00%)

--- problems/0392-is-subsequence/solution.py
+++ problems/0392-is-subsequence/solution.py
@@ -1,26 +1,14 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        '''
-            Example 1 
-                s = 'abc'
-                t = 'ahbgdc'
-
-                idx == ??
-                
-
-        '''
-        i = j = 0
-        con = ''
-        
-        if not s:
+        i,j =0, 0
+        cnt = []
+        while i < len(s) and j < len(t):
+                if s[i] == t[j]:
+                    cnt.append(s[i])
+                    i+=1
+                    j+=1
+                else:
+                    j+=1
+        if ''.join(cnt) == s:
             return True
-
-        while j < len(t) and i < len(s):
-            if s[i] == t[j]:
-                con+=t[j]
-                j+=1
-                i+=1
-            else:
-                j+=1
-        return s == con
-
+        return False
