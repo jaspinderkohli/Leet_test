be9722c4e3a55de852dba84f5e771ea457534ee8
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2022-11-11 01:42:05 +0000

    [LeetCode Sync] Runtime - 1741 ms (5.00%), Memory - 14.2 MB (100.00%)

--- problems/0322-coin-change/solution.py
+++ problems/0322-coin-change/solution.py
@@ -1,14 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # x amnt because sare amounts lai dekhna kine coins chahide aa
-        dp = [float('inf')] * (amount+1)
+
+        # @lru_cache(None)  
+        # def dfs(rem):
+        #     if rem < 0:
+        #         return -1
+        #     if rem == 0:
+        #         return 0
+        #     min_cost = float('inf')
+        #     for coin in coins:
+        #         res = dfs(rem - coin)
+        #         if res != -1:
+        #             min_cost = min(min_cost, res + 1)
+        #     return min_cost if min_cost != float('inf') else -1
+        # return dfs(amount)
+
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
-
-        for i in range(1, amount+1):
-            for coin in coins:
-                if coin <= i:
-                    dp[i] = min(dp[i], dp[i-coin]+1) #i-1 == 0
-
-        return dp[amount] if dp[amount] != float('inf') else -1
-
-
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1 
+            
+                    
