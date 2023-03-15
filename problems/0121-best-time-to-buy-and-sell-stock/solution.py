82c6a85b5467f9c7f09b33fef659839c305c9471
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-03-15 20:40:02 +0000

    Sync LeetCode submission Runtime - 1135 ms (5.01%), Memory - 25 MB (99.73%)

--- problems/0121-best-time-to-buy-and-sell-stock/solution.py
+++ problems/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # initialize the minimum price seen so far and the maximum profit seen so far to 0
-        min_price = float('inf')
+        n = len(prices)
+        if n < 2:
+            return 0
+        min_price = prices[0]
         max_profit = 0
-
-        for price in prices:
-            # update the minimum price seen so far
-            min_price = min(min_price, price)
-
-            # calculate the potential profit if the stock is sold at the current price
-            profit = price - min_price
-
-            # update the maximum profit seen so far if the current potential profit is larger than the current maximum profit
-            max_profit = max(max_profit, profit)
-
+        for i in range(1, n):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
         return max_profit
+
+
+        
+
