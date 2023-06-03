d8f7745cfc08c8fd9773cc188484345fab5842b2
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-06-03 20:08:34 +0000

    [LeetCode Sync] Runtime - 83 ms (5.68%), Memory - 16.4 MB (99.84%)

--- problems/1580-shuffle-the-array/solution.py
+++ problems/1580-shuffle-the-array/solution.py
@@ -1,21 +1,14 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        # '''
-        #     keep 2 counters 1 for ans and 1 for nums
-        # '''
-        # ans = len(nums) * [None]
-        # i=cnt=0
-        # while i < n:
-        #     ans[cnt],ans[cnt+1] = nums[i], nums[i+n]
-        #     i+=1
-        #     cnt+=2
-        # return ans
-        new = 2 * n * [None]
-        cnt = 0
-        for i in range(0,n):
-            new[cnt], new[cnt+1] = nums[i], nums[i+n]
+        '''
+            keep 2 counters 1 for ans and 1 for nums
+        '''
+        ans = len(nums) * [None]
+        i=cnt=0
+        while i < n:
+            ans[cnt],ans[cnt+1] = nums[i], nums[i+n]
+            i+=1
             cnt+=2
-        return new
-
+        return ans
 
 
