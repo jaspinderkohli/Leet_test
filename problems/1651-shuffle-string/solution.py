eff6ddee4f7e0d0bba96220db07d8e20ed498c7d
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-06-05 14:02:15 +0000

    Sync LeetCode submission Runtime - 63 ms (8.65%), Memory - 16.5 MB (41.47%)

---
+++ problems/1651-shuffle-string/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        new_s = len(s) * [0]
+        for i,x in enumerate(indices):
+            new_s[x] = s[i]
+        return ''.join(new_s)
