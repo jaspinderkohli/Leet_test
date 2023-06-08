ded1fbf48e046eb32ecef24e5497b81af325f1ce
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-06-08 20:00:47 +0000

    [LeetCode Sync] Runtime - 47 ms (29.39%), Memory - 16.3 MB (99.43%)

--- problems/0069-sqrtx/solution.py
+++ problems/0069-sqrtx/solution.py
@@ -5,19 +5,17 @@ class Solution:
             8
             2 ** 2
             3 ** 3
+            
+            First we need to search for minimal k satisfying condition k^2 > x, 
+            then k - 1 is the answer to the question. We can easily come up with the solution. Notice that I set right = x + 1 instead of right = x to deal with special input cases like x = 0 and x = 1.
         '''
-        if x == 0:
-            return 0
-        elif x == 1:
-            return 1
-        else:
-            l = 1 
-            h = x
-            while (l <= h):
-                mid = (h + l) // 2
-                if ( mid ** 2 <= x < (mid+1)**2):
-                    return mid
-                elif (mid ** 2 > x):
-                    h = mid -1
-                else:
-                    l = mid + 1
+        l, r = 0 , x+1
+        while l < r:
+            mid = (l+r) // 2
+            if mid * mid > x:
+                r = mid
+            else:
+                l = mid + 1
+        return l-1
+
+
