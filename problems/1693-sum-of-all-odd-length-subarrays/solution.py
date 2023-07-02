7372b6487108ad45f0b27466ace28fcb2e75137b
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-07-02 22:20:16 +0000

    Sync LeetCode submission Runtime - 106 ms (5.25%), Memory - 16.2 MB (99.78%)

---
+++ problems/1693-sum-of-all-odd-length-subarrays/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        n = len(arr)
+        tot_sum = 0
+        for i in range(n):
+            for j in range(i,n):
+                subarray = arr[i:j+1]
+                if len(subarray) %2 ==1:
+                    tot_sum += sum(subarray)
+        return tot_sum
+        
+
