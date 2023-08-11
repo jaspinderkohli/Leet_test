5c077fc2f2353cf68a9422b1ac4549ec4d110233
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-08-11 14:54:40 +0000

    [LeetCode Sync] Runtime - 36 ms (47.45%), Memory - 16.2 MB (99.35%)

--- problems/1205-defanging-an-ip-address/solution.py
+++ problems/1205-defanging-an-ip-address/solution.py
@@ -1,12 +1,5 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
     
-        # deranged_ip = address.replace('.','[.]')
-        # return deranged_ip
-        deranged_ip = ''
-        for i in range(len(address)):
-            if address[i] != '.':
-                deranged_ip += address[i]
-            else:
-                deranged_ip += '[.]'
+        deranged_ip = address.replace('.','[.]')
         return deranged_ip
