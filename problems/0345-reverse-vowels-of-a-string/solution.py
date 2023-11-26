c79578ccb75866e24808da2512195e6e67d96ff7
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2023-11-26 20:31:44 +0000

    [LeetCode Sync] Runtime - 39 ms (94.96%), Memory - 17.3 MB (83.55%)

--- problems/0345-reverse-vowels-of-a-string/solution.py
+++ problems/0345-reverse-vowels-of-a-string/solution.py
@@ -1,36 +1,20 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        '''
-            Example 1 
-                s = "hello"
-                      ^  ^
-                output = "holle"
-
-            Example 2
-                s = "leetcode"
-                      ^     ^
-                      leetcode
-                        ^  ^
-                      leotcede
-                            break
-                output = leotcede
-        '''
-        vowels = ['a','e','i','o','u','A','E','I', 'O', 'U']
-        i, j = 0 , len(s)-1
+        vowels = set("aeiouAEIOU")
         s = list(s)
-        while i < j:
-            if s[i] not in vowels:
-                i+=1
-                continue
-            if s[j] not in vowels:
-                j-=1
-                continue
+        left, right = 0, len(s) - 1
 
-            if s[i] in vowels and s[j] in vowels:
-                s[i], s[j] = s[j], s[i]
-                i+=1
-                j-=1
+        while left < right:
+            if s[left] not in vowels:
+                left += 1
+                continue
+            if s[right] not in vowels:
+                right -= 1
+                continue
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
         return ''.join(s)
-            
 
 
