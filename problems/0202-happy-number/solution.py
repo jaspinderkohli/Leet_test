4d9a82d3723310af33e377904bab778ae720ae92
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2024-01-07 21:50:15 +0000

    [LeetCode Sync] Runtime - 40 ms (36.82%), Memory - 17.4 MB (7.61%)

--- problems/0202-happy-number/solution.py
+++ problems/0202-happy-number/solution.py
@@ -1,25 +1,40 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # Define the happy_number function that takes a number and a set of visited numbers as input
-        def happy_number(n, visited=set()):
-            # Initialize the total sum to 0
-            total_sum = 0
-            # Iterate over the digits of the number
-            for digit in str(n):
-                # Add the square of each digit to the total sum
-                total_sum += int(digit) ** 2
-            # If the total sum is 1, return True (the number is happy)
-            if total_sum == 1:
-                return True
-            # If the total sum has been seen before, return False (there is a cycle)
-            elif total_sum in visited:
-                return False
-            # Otherwise, add the total sum to the set of visited numbers and recurse with the new number
-            else:
-                visited.add(total_sum)
-                return happy_number(total_sum, visited)
+        # # Define the happy_number function that takes a number and a set of visited numbers as input
+        # def happy_number(n, visited=set()):
+        #     # Initialize the total sum to 0
+        #     total_sum = 0
+        #     # Iterate over the digits of the number
+        #     for digit in str(n):
+        #         # Add the square of each digit to the total sum
+        #         total_sum += int(digit) ** 2
+        #     # If the total sum is 1, return True (the number is happy)
+        #     if total_sum == 1:
+        #         return True
+        #     # If the total sum has been seen before, return False (there is a cycle)
+        #     elif total_sum in visited:
+        #         return False
+        #     # Otherwise, add the total sum to the set of visited numbers and recurse with the new number
+        #     else:
+        #         visited.add(total_sum)
+        #         return happy_number(total_sum, visited)
         
-        # Call the happy_number function with the input number and return the result
+        # # Call the happy_number function with the input number and return the result
+        # return happy_number(n)
+        def happy_number(n, visited=set()):
+            tot_sum = 0
+            for dig in str(n):
+                tot_sum += int(dig) ** 2
+            
+            if tot_sum == 1:
+                return True
+            elif tot_sum in visited:
+                return False
+
+            else:
+                visited.add(tot_sum)
+                return happy_number(tot_sum, visited)
+        
         return happy_number(n)
 
 
