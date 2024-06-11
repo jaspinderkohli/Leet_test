699610fdf37eef05a5499cdd1c4878adad83d109
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2024-06-11 23:17:29 +0000

    Sync LeetCode submission Runtime - 36 ms (81.86%), Memory - 16.6 MB (86.48%)

---
+++ problems/1217-relative-sort-array/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        '''
+            arr1 = [2,3,1,3,2,4,6,7,9,2,19]
+            arr2 = [2,1,4,3,9,6]
+        '''
+        max_element = max(arr1)
+        count = [0] * (max_element + 1)
+
+        # Count occurrences of each element
+        for element in arr1:
+            count[element] += 1
+
+        # Add elements as per relative order
+        result = []
+        for value in arr2:
+            while count[value] > 0:
+                result.append(value)
+                count[value] -= 1
+
+        # Add remaining elements in ascending order
+        for num in range(max_element + 1):
+            while count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+
+        return result
