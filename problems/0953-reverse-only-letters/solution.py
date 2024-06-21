be2e0a5a57c3bc8cab4909089c07a16fc5bb1371
Jaspinder Singh <36575352+jaspinderkohli@users.noreply.github.com>
2024-06-21 19:50:51 +0000

    Sync LeetCode submission Runtime - 38 ms (35.47%), Memory - 16.4 MB (70.40%)

---
+++ problems/0953-reverse-only-letters/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        # print(ord('a'))
+
+        l, r = 0, len(s) - 1
+        s = list(s)
+        
+        while l < r:
+            if not s[l].isalpha():
+                l+=1
+            elif not s[r].isalpha():
+                r-=1
+            else: 
+                s[l], s[r] = s[r], s[l]
+                l+=1
+                r-=1
+        return ''.join(s)
+        
+
+
+
